ab41f7d2e1d924d3b1c9de0fd190c137c5725d70
nolanding <deekshachandvani@gmail.com>
2019-04-10 19:34:58 +0530

    added all files

---
+++ decryption.py
@@ -0,0 +1,18 @@
+from Crypto import Random
+from Crypto.Cipher import AES
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
+def decrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt(ciphertext, key)
+    r=dec.decode("utf-8")
+    print(r)
+key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+decrypt_file('test.txt.enc',key)
